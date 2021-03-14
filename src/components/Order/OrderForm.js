6a8ee5a9038d123f460cba908406489a3818b9ca
Dex <dexterallengalvez@gmail.com>
2021-03-14 21:42:55 +1100

    Created a useForm custom hook

--- src/components/Order/OrderForm.js
+++ src/components/Order/OrderForm.js
@@ -1,35 +1,77 @@
 import { Grid } from "@material-ui/core";
-import React from "react";
+import React, { useState, setValues } from "react";
 import Form from "../../layouts/Form";
 import { Button, Input, Select } from "../../controls";
+import useForm from "../../hooks/useForm";
+
+const generateOrderNumber = () =>
+  Math.floor(100000 + Math.random() * 900000).toString();
+
+const getFreshModelObject = () => ({
+  orderMasterId: 0,
+  orderNumber: generateOrderNumber(),
+  customerId: 1,
+  paymentMethod: "none",
+  grandTotal: 0,
+  deletedOrderItemIds: "",
+  orderDetails: [],
+});
 
 export default function OrderForm() {
+  //Initialize the custom hook
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetFormControls,
+  } = useForm(getFreshModelObject);
+
   return (
     <Form>
       <Grid container>
         <Grid item xs={6}>
-          <Input label="Order Number" name="orderNumber" disabled></Input>
+          <Input
+            disabled
+            label="Order Number"
+            name="orderNumber"
+            value={values.orderNumber}
+          ></Input>
           <Select
             label="Customer"
             name="customerId"
             options={customerOptions}
+            value={values.customerId}
+            onChange={handleInputChange}
           />
         </Grid>
         <Grid item xs={6}>
-          <Input label="Grand Total" name="grantTotal" disabled></Input>
+          <Input
+            disabled
+            label="Grand Total"
+            name="grantTotal"
+            value={values.grandTotal}
+          ></Input>
           <Select
             label="Payemnt Method"
             name="paymentMethod"
             options={paymentMethods}
+            value={values.paymentMethod}
+            onChange={handleInputChange}
           />
         </Grid>
-        <Button>Button</Button>
+        <Button onClick={resetFormControls}>Reset</Button>
       </Grid>
     </Form>
   );
 }
 
 const customerOptions = [
+  {
+    id: 0,
+    title: "Select",
+  },
   {
     id: 1,
     title: "Customer1",
